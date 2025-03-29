56a062348b42fe268de4f177cccd23df2495ac48
Mawais200 <fa22-bsse-201@lgu.edu.pk>
2025-03-29 23:36:30 +0500

    write a code that add a element in linklist

--- LL.java
+++ LL.java
@@ -1,14 +1,46 @@
+package Javalearning16;
+
 public class LL {
-    class Node{
+    private Node head;
+
+    class Node {
         String data;
         Node next;
-        Node (String data){
+
+        Node(String data) {
             this.data = data;
-            this.next=null;
+            this.next = null;
         }
     }
-   
+
+    // Add data to the front of the list
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Print out the contents of the linked list
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        LL list= new LL();
+        LL list = new LL();
+        list.addFirst("Apple");
+        list.addFirst("Banana");
+        list.addFirst("Cherry");
+        list.printList(); // Output: Cherry Banana Apple
     }
 }
+
+
